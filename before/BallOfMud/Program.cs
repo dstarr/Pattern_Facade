349a32e6f27d46ab6c4bada973e0e2d1ac5e66b8
David Starr <david@elegantcode.com>
2019-11-10 05:07:57 -0800

    dded ball of mud - first commit

---
+++ before/BallOfMud/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BallOfMud.Services;
+
+namespace BallOfMud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigClass bigClass = new BigClass();
+            
+            bigClass.SetValue(3);
+            bigClass.IncrementI();
+            bigClass.IncrementI();
+            bigClass.IncrementI();
+            
+            bigClass.Decrememnt();
+
+            Console.WriteLine($"Final Number : {bigClass.GetValueA()}");
+        }
+    }
+}
